cf31b0dd2c4c63a83e09aee7f7ad685cb0fd10c0
Choi Yong Won <cywin0821@gmail.com>
2025-12-24 15:20:37 +0900

    feat: add Jest configuration and testing scripts for backend and frontend
    - Introduced Jest configuration files for backend and frontend applications.
    - Updated package.json files to include testing scripts and type-checking commands.
    - Enhanced turbo.json with test dependencies and caching settings.
    - Added necessary Jest dependencies and types for improved testing capabilities.

---
+++ apps/jest.config.base.js
@@ -0,0 +1,7 @@
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s', '!**/*.spec.ts', '!**/node_modules/**'],
+};
