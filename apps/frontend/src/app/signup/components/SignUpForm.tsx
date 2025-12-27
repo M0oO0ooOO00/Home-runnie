73ae7a35997ccf6711614be313aab7687e789ab5
chyun7114 <chyun7114@naver.com>
2025-12-27 18:07:13 +0900

    Fix: 회원가입 페이지 헤더 두번 나오던 문제 해결

--- apps/frontend/src/app/signup/components/SignUpForm.tsx
+++ apps/frontend/src/app/signup/components/SignUpForm.tsx
@@ -4,13 +4,8 @@ import Header from '@/shared/ui/header/header';
 
 export default function SignUpForm() {
   return (
-    <>
-      <Header />
-      <div className="w-[460px] m-auto flex flex-col justify-start items-center gap-14">
-        {/*<div className="gap-12 w-full items-center flex flex-col">*/}
-        <TextFields baseBallTeamItems={baseBallTeamItems} genderItems={genderItems} />
-        {/*</div>*/}
-      </div>
-    </>
+    <div className="w-[460px] m-auto flex flex-col justify-start items-center gap-14">
+      <TextFields baseBallTeamItems={baseBallTeamItems} genderItems={genderItems} />
+    </div>
   );
 }
