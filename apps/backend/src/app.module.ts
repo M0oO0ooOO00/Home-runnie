890bdc3263de77f8f9702ed26bdeb647dbad911f
Choi Yong Won <cywin0821@gmail.com>
2025-12-31 17:38:46 +0900

    feat: app에 모듈 추가

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -12,29 +12,31 @@ import { ParticipationModule } from './participation/participation.module';
 import { ScrapModule } from './scrap/scrap.module';
 import { AdminModule } from './admin/admin.module';
 import { WarnModule } from './warn/warn.module';
+import { ChatModule } from './chat/chat.module';
 import databaseConfig from './common/config/database.config';
 import * as path from 'path';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: [path.resolve(process.cwd(), 'secret/.env')],
-            isGlobal: true,
-            load: [databaseConfig],
-        }),
-        DbModule,
-        WarnModule,
-        MemberModule,
-        AuthModule,
-        ReportModule,
-        PostModule,
-        ParticipationModule,
-        ScrapModule,
-        AdminModule,
-    ],
-    providers: [
-        { provide: APP_FILTER, useClass: HttpExceptionFilter },
-        { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
-    ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [path.resolve(process.cwd(), 'secret/.env')],
+      isGlobal: true,
+      load: [databaseConfig],
+    }),
+    DbModule,
+    WarnModule,
+    MemberModule,
+    AuthModule,
+    ReportModule,
+    PostModule,
+    ParticipationModule,
+    ScrapModule,
+    AdminModule,
+    ChatModule,
+  ],
+  providers: [
+    { provide: APP_FILTER, useClass: HttpExceptionFilter },
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+  ],
 })
 export class AppModule {}
