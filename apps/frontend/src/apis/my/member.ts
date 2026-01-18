ee2fcbcd37d5cf118996dca5ee313cc327544624
chyun7114 <chyun7114@naver.com>
2026-01-18 16:39:03 +0900

    Feat: Fetch API Wrapper 클래스 모든 API 호출에 적용

--- apps/frontend/src/apis/my/member.ts
+++ apps/frontend/src/apis/my/member.ts
@@ -1,16 +1,7 @@
 import { GetMyProfileResponse } from '@homerunnie/shared';
 
+import { apiClient } from '@/lib/fetchClient';
+
 export const getMyProfile = async (): Promise<GetMyProfileResponse> => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
-
-  const response = await fetch(`${apiUrl}/member/my`, {
-    method: 'GET',
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    throw new Error('멤버 정보를 불러오는것에 실패했습니다.');
-  }
-
-  return response.json();
+  return apiClient.get('/member/my');
 };
--- apps/frontend/src/apis/my/scrappedRecruitment.ts
+++ apps/frontend/src/apis/my/scrappedRecruitment.ts
@@ -1,17 +1,9 @@
 import { PagePaginationResponse, RecruitmentSummaryResponse } from '@homerunnie/shared';
 
+import { apiClient } from '@/lib/fetchClient';
+
 export const getScrappedRecruitments = async (
   pageNum: number,
 ): Promise<PagePaginationResponse<RecruitmentSummaryResponse[]>> => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
-  const response = await fetch(`${apiUrl}/member/my/scrapped-recruitments?page=${pageNum}`, {
-    method: 'GET',
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    throw new Error('스크랩한 모집글 조회에 실패했습니다.');
-  }
-
-  return response.json();
+  return apiClient.get(`/member/my/scrapped-recruitments?page=${pageNum}`);
 };
