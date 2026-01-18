6a5b497dfbd803a65a99faa362140cef271b38e9
Choi Yong Won <cywin0821@gmail.com>
2026-01-18 14:59:21 +0900

    feat: 디비 모듈 추가

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -3,8 +3,10 @@ import { ChatGateway } from '@/chat/chat.gateway';
 import { ChatService } from '@/chat/service';
 import { ChatRepository } from '@/chat/repository';
 import { ChatController } from '@/chat/controller';
+import { DbModule } from '@/common/db/db.module';
 
 @Module({
+  imports: [DbModule],
   controllers: [ChatController],
   providers: [ChatGateway, ChatService, ChatRepository],
   exports: [ChatService],
