5327ad5da7e31e03be5dc5ca8aeb4cf268db4932
Choi Yong Won <cywin0821@gmail.com>
2026-01-18 14:46:39 +0900

    feat: 채팅방 로직에 tx를 이용하도록 수정

--- apps/backend/src/chat/service/chat.service.ts
+++ apps/backend/src/chat/service/chat.service.ts
@@ -1,15 +1,33 @@
 import { Inject, Injectable } from '@nestjs/common';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
 import { ChatRepository } from '@/chat/repository';
 import { ChatRoomResponseDto, GetChatRoomsResponseDto } from '@/chat/dto/response';
 import { ChatRoomMemberRole } from '@homerunnie/shared';
+import { DATABASE_CONNECTION } from '@/common';
+import * as schema from '@/common/db/schema';
+
+type DbType = NodePgDatabase<typeof schema>;
 
 @Injectable()
 export class ChatService {
-  constructor(@Inject() private readonly chatRepository: ChatRepository) {}
+  constructor(
+    @Inject() private readonly chatRepository: ChatRepository,
+    @Inject(DATABASE_CONNECTION) private readonly db: DbType,
+  ) {}
 
   async createChatRoom(postId: number, memberId: number): Promise<ChatRoomResponseDto> {
-    const chatRoom = await this.chatRepository.createChatRoom(postId);
-    await this.chatRepository.createChatRoomMember(chatRoom.id, memberId, ChatRoomMemberRole.HOST);
+    const chatRoom = await this.db.transaction(async (tx) => {
+      const newChatRoom = await this.chatRepository.createChatRoom(postId, tx);
+
+      await this.chatRepository.createChatRoomMember(
+        newChatRoom.id,
+        memberId,
+        ChatRoomMemberRole.HOST,
+        tx,
+      );
+
+      return newChatRoom;
+    });
 
     return ChatRoomResponseDto.from(chatRoom);
   }
@@ -23,6 +41,7 @@ export class ChatService {
       this.chatRepository.findChatRoomsByMemberId(memberId, page, limit),
       this.chatRepository.countChatRoomsByMemberId(memberId),
     ]);
+
     return GetChatRoomsResponseDto.from(chatRooms, total, page, limit);
   }
 }
