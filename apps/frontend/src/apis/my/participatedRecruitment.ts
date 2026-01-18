b33a1b491d2b045b9c38ae568969a4ff0c134006
chyun7114 <chyun7114@naver.com>
2026-01-18 16:39:03 +0900

    Feat: Fetch API Wrapper 클래스 모든 API 호출에 적용

--- apps/frontend/src/apis/my/participatedRecruitment.ts
+++ apps/frontend/src/apis/my/participatedRecruitment.ts
@@ -1,17 +1,9 @@
 import { PagePaginationResponse, RecruitmentSummaryResponse } from '@homerunnie/shared';
 
+import { apiClient } from '@/lib/fetchClient';
+
 export const getParticipatedRecruitments = async (
   pageNum: number,
 ): Promise<PagePaginationResponse<RecruitmentSummaryResponse[]>> => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
-  const response = await fetch(`${apiUrl}/member/my/participated-recruitments?page=${pageNum}`, {
-    method: 'GET',
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    throw new Error('내가 참여한 모집글 조회에 실패했습니다.');
-  }
-
-  return response.json();
+  return apiClient.get(`/member/my/participated-recruitments?page=${pageNum}`);
 };
